400944caa24e212d62ca74ab4b94e50e5035787d
XPerianer <dominik.meier@student.hpi.de>
2021-06-17 23:09:50 +0200

    Add more necessary files

---
+++ docker-run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+docker build \
+	--network host \
+	-t fabseal .
+
+docker run \
+	--mount type=bind,source="$(pwd)"/data,target=/fabseal/data \
+	-it \
+	fabseal
+
